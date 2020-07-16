3a3b91abe9afe40e0b6167c3c0f0b8efd3447790
Russell Sutter <russell.sutter@gmail.com>
2020-07-16 21:32:20 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +127,132 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(given_name)
+  
+  points = nil
+  
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if given_name == player[:player_name]
+            points = player[:points]
+          end
+        end
+      end
+    end
+  end
+  return points
+end
+
+
+def shoe_size(given_name)
+
+  shoe_size = nil
+  
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if given_name == player[:player_name]
+            shoe_size = player[:shoe]
+          end
+        end
+      end
+    end
+  end
+  return shoe_size
+end
+
+
+def team_colors(given_team_name)
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if data == given_team_name
+        return game_hash[homeaway][:colors]
+      end
+    end
+  end
+end
+  
+  
+def team_names
+  team_names_array = []
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if attribute == :team_name
+        team_names_array << data
+      end
+    end
+  end
+  return team_names_array
+end
+  
+
+def player_numbers(given_team_name)
+  player_numbers_array = []
+  game_hash.each do |homeaway, teamspecs|
+    if teamspecs[:team_name] == given_team_name
+      teamspecs.each do |attribute, data|
+        if attribute == :players
+          data.each do |player|
+            player_numbers_array << player[:number]
+          end
+        end
+      end
+    end
+  end
+  return player_numbers_array
+end
+  
+def player_stats(given_player_name)
+  
+  player_stats = {}
+  
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == given_player_name
+            player_stats[:player_name] = player[:player_name]
+            player_stats[:number] = player[:number]
+            player_stats[:shoe] = player[:shoe]
+            player_stats[:points] = player[:points]
+            player_stats[:rebounds] = player[:rebounds]
+            player_stats[:assists] = player[:assists]
+            player_stats[:steals] = player[:steals]
+            player_stats[:blocks] = player[:blocks]
+            player_stats[:slam_dunks] = player[:slam_dunks]  #seems like there should be an easier way to return these hashes instead of having list every single one, and then add into a new hash
+          end
+        end
+      end
+    end
+  end
+  return player_stats
+end
+  
+def big_shoe_rebounds
+  
+  shoe_size = 0
+  rebounds = 0
+  
+  game_hash.each do |homeaway, teamspecs|
+    teamspecs.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:shoe] > shoe_size
+            shoe_size = player[:shoe]
+            rebounds = player[:rebounds]
+          end
+        end
+      end
+    end
+  end
+  return rebounds
+end
+
+
+
+
+
+
