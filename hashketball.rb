b02c2f0d23a63c7d44f01d11d4ee241ac278f842
Vincent Collis <vinsanity0050@gmail.com>
2020-07-26 06:17:44 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +128,151 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(name)
+  points = 0
+  
+  #check home team
+  #iterate over player names
+  
+  i=0
+  while i < game_hash[:home][:players].length
+    if game_hash[:home][:players][i][:player_name] == name
+      points = game_hash[:home][:players][i][:points]
+    end
+    if points != 0
+      break
+    end
+    i+=1
+  end
+  
+  i=0
+  if points == 0
+    while i < game_hash[:away][:players].length
+      if game_hash[:away][:players][i][:player_name] == name
+        points = game_hash[:away][:players][i][:points]
+      end
+      if points != 0
+        break
+      end
+      i+=1
+    end
+  end  
+
+  if points == 0
+    points = "Player does no exisit"
+  end
+  points
+end
+
+def shoe_size(name)
+  size = 0
+  
+  i=0
+  while i < game_hash[:home][:players].length
+    if game_hash[:home][:players][i][:player_name] == name
+      size = game_hash[:home][:players][i][:shoe]
+    end
+    if size != 0
+      break
+    end
+    i+=1
+  end
+  
+  i=0
+  while i < game_hash[:away][:players].length
+    if game_hash[:away][:players][i][:player_name] == name
+      size = game_hash[:away][:players][i][:shoe]
+    end
+    if size != 0
+      break
+    end
+    i+=1
+  end
+  
+  if size == 0
+    size = "Player does no exisit"
+  end
+  size
+  #for each player save shoe size into array
+end
+
+def team_colors(name)
+
+  if game_hash[:home][:team_name] == name
+    colors = game_hash[:home][:colors]
+  end
+  
+  if game_hash[:away][:team_name] == name
+    colors = game_hash[:away][:colors]
+  end
+  
+  colors
+
+end
+
+def team_names()
+  teams = []
+  teams.push(game_hash[:home][:team_name])
+  teams.push(game_hash[:away][:team_name])
+  teams
+end
+
+def player_numbers(team)
+  nums = []
+  
+  if game_hash[:home][:team_name] == team
+    i=0
+    while i < game_hash[:home][:players].length
+      nums.push(game_hash[:home][:players][i][:number])
+      i+=1
+    end
+  elsif game_hash[:away][:team_name] == team
+      i=0
+    while i < game_hash[:away][:players].length
+      nums.push(game_hash[:away][:players][i][:number])
+      i+=1
+    end
+  end
+  nums
+end
+
+def player_stats(name)
+  i=0
+  while i<game_hash[:home][:players].length
+    if game_hash[:home][:players][i][:player_name] == name
+      stats = game_hash[:home][:players][i]
+    end
+    i+=1
+  end
+  
+  i=0
+  while i<game_hash[:away][:players].length
+    if game_hash[:away][:players][i][:player_name] == name
+      stats = game_hash[:away][:players][i]
+    end
+    i+=1
+  end
+  stats
+end
+
+def big_shoe_rebounds()
+  new_arr = []
+  game_hash[:home][:players].each do |key|
+    new_arr.push(key[:shoe])
+  end
+  game_hash[:away][:players].each do |key|
+    new_arr.push(key[:shoe])
+  end
+  
+  max_rebounds = new_arr.max()
+  max_rebounds_index = new_arr.index(max_rebounds)
+  
+  
+  if max_rebounds_index <= game_hash[:home][:players].length
+    output = game_hash[:home][:players][max_rebounds_index][:rebounds]
+  else
+    output = game_hash[:away][:players][max_rebounds_index][:rebounds]
+  end
+    
+  output
+end
